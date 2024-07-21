3e1d47cc6fbf37f750dc69963c2687acf8b864ef
archi-innovacii <archi.rosinnovacii@gmail.com>
2024-07-21 19:01:21 +0300

    add scraper

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# web-scraper
-web-scraper
+# Simple Web scraper
+
+This is a simple web scraper written in Go. It accepts a list of URLs as input, crawls these pages, extracts all the links (URLs) from each page, and outputs the unique URLs found.
+
+## Features
+
+- Parallel crawling using goroutines
+- Extraction of links from HTML
+- Collection of unique URLs
+- Simple and efficient design
+
+## Requirements
+
+- Go 1.16 or later
+
+## Installation
+
+1. Clone the repository:
+
+   ```sh
+   git clone https://github.com/yakuznecov/web-scraper.git
+   cd web-scraper
+   ```
+
+2. Build the project:
+
+   ```sh
+   go build -o scraper
+   ```
+
+## Usage
+
+To run the scraper, provide a list of seed URLs as arguments:
+
+```sh
+./scraper http://example.com http://example.org
+
+```
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func getHref(t html.Token)(ok bool, href string) {
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+
+	return
+}
+
+func crawl(url string, ch chan string, chFinished chan bool) {
+	resp, err := http.Get(url) // получить ответ
+
+	// функция с defer вызывается в конце этой функции, отложенная функция
+	defer func() {
+		chFinished <- true
+	}()
+
+	if err != nil {
+		fmt.Println("Не удалось выполнить сканирование", url)
+		return
+	}
+
+	b := resp.Body // получить тело ответа
+	defer b.Close() // закрыть тело ответа после выполнения функции
+
+	z := html.NewTokenizer(b) // токенизатор html
+
+	for{
+		tt := z.Next() // получить следующий токен
+
+		switch {
+		case tt == html.ErrorToken:
+			return
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+		isAnchor := t.Data == "a"
+
+		if !isAnchor {
+			continue
+		}
+
+		ok, url := getHref(t)
+
+		if !ok {
+			continue
+		}
+
+		hasProto := strings.Index(url, "http") == 0 // проверка на протокол
+
+		if hasProto {
+			ch <- url // публикация на сайте уникальных url-адресов
+		}
+	}
+	}
+}
+
+func main() {
+	foundUrls := make(map[string]bool) // найденные url-адреса
+	seedUrls := os.Args[1:] // Получение аргументов командной строки 
+
+	chUrls := make(chan string) // канал url
+	chFinished := make(chan bool) // канал завершения работы
+
+	// разбор всех url адресов
+	for _, url := range seedUrls {
+		go crawl(url, chUrls, chFinished)
+	}
+
+	for c := 0; c < len(seedUrls); {
+		select {
+		case url := <- chUrls:
+			foundUrls[url] = true
+		case <- chFinished:
+			c++
+		}
+	}
+
+	fmt.Println("\nFound", len(foundUrls), "unique links:\n")
+
+	// распечатка всех адресов
+	for url, _ := range foundUrls {
+		fmt.Println("- " + url)
+	}
+
+	// закрыть канал
+	close(chUrls)
+}
